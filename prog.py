d0bdd92b418539ea01fd38683f1367733c29e798
SAEED <saeedhisbani@yahoo.co.uk>
2023-10-11 08:57:08 +0500

    user 1 func 2

--- prog.py
+++ prog.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 def main():
     print('User 1 Main Function')
-#<<<<<<< HEAD
     user2func1()
     user1func1()
+    user1func2()
 
 def user2func1():
     print('user 2 func 1')
-#=======
 def user1func1():
     print('user 1 func 1')
-#>>>>>>> 3cd6653 (user 1 func 1)
+def user1func2():
+    print('user 1 func 2')
 main()
