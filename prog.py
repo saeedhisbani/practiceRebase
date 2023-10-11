28f6e11d202e65869034c209ab874cde0d5f5f5a
SAEED <saeedhisbani@yahoo.co.uk>
2023-10-11 16:23:54 +0500

    user 2 func 3

--- prog.py
+++ prog.py
@@ -3,20 +3,22 @@ def main():
     print('User 1 Main Function')
     user1func1()
     user1func2()
+    print('-------------------------------------------')
     user2func1()
     user2func2()
+    user2func3()
 
-def user2func1():
-    print('user 2 func 1')
 def user1func1():
     print('user 1 func 1')
 def user1func2():
     print('user 1 func 2')
+
 def user2func1():
     print('user 2 func 1')
 def user2func2():
     print('user 2 func 2')
-def user1func1():
-    print('user 1 func 1')
+def user2func3():
+    print('user 2 func 3')
+
 main()
 
